4cff3f6de98dfac262cf176ed6e2b0c5d75aa5ff
Sibbons Shrestha <Sibbonsss@gmail.com>
2019-08-23 11:33:06 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send('yeet');
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(3000);
