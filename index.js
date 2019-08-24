b3887f09938b8b157b6305e83237a487db09135a
Sibbons Shrestha <Sibbonsss@gmail.com>
2019-08-23 21:57:53 -0700

    finished auth flow

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
-const app = express()
+const app = express();
+const mongooose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys.js');
 
+require('./models/User');
+require('./services/passport.js');
 
-app.get('/', (req, res) => {
-    res.send('yeet');
-})
+mongooose.connect(keys.mongoURI, { useNewUrlParser: true });
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
